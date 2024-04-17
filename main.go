2a78f33531f8e6025aa882073b873eea26df83e9
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2024-04-17 18:02:15 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,4 +9,13 @@ func main() {
 	fmt.Println("Go REST API v1.0")
 
 	mux := http.NewServeMux()
+
+	// OLD
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+		fmt.Println(err.Error())
+	}
 }
