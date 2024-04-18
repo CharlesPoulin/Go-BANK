67001eaeeb4a7fac6644ee55a5d0f467675b3903
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2024-04-18 15:10:22 -0400

    least important func at the bot of page

--- api.go
+++ api.go
@@ -8,39 +8,6 @@ import (
 	"net/http"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	return json.NewEncoder(w).Encode(v)
-}
-
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
-type apiError struct {
-	error string
-}
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			err := WriteJSON(w, http.StatusBadRequest, apiError{error: err.Error()})
-			if err != nil {
-				return
-			}
-		}
-	}
-}
-
-type APIServer struct {
-	listenAddr string
-}
-
-func NewAPIServer(listenAddr string) *APIServer {
-	return &APIServer{
-		listenAddr: listenAddr,
-	}
-}
-
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
@@ -89,4 +56,37 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type apiError struct {
+	error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			err := WriteJSON(w, http.StatusBadRequest, apiError{error: err.Error()})
+			if err != nil {
+				return
+			}
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
 }
